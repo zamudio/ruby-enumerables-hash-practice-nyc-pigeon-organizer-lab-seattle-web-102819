2e0359382ab11f783f0b9faeb3d5f3a94fdd39ac
Michael Z <mziacm@gmail.com>
2019-10-15 05:05:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,26 +15,3 @@ def nyc_pigeon_organizer(data)
   end
   pigeon_hash
 end
-
-
-#def nyc_pigeon_organizer(data)
-#  new_hash = {}
-#  data.each do |key, value|
-#    value.each do |new_value, names|
-#      names.each do |name|
-#        
-#        if !new_hash[name]
-#          new_hash[name] = {}
-#        end
-#
-#        if !new_hash[name][key]
-#          new_hash[name][key] = []
-#        end
-#
-#        new_hash[name][key] << new_value.to_s
-#
-#      end
-#    end
-#  end
-#  new_hash
-#end
